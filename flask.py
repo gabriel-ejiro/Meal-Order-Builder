1bc0469c7217e5dc4a316b5b94e89b3856401b01
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-05-20 17:57:28 +0200

    Update flask.py

--- flask.py
+++ flask.py
@@ -6,3 +6,16 @@ from flask import Flask, request, jsonify
 from threading import Thread
 
 app = Flask(__name__)
+
+
+# Cell 14: Helper function to jsonify Meal
+def meal_to_dict(meal):
+    return {
+        "main": meal.main,
+        "side": meal.side,
+        "drink": meal.drink,
+        "dessert": meal.dessert,
+        "total_price": sum(part["price"] for part in [meal.main, meal.side, meal.drink, meal.dessert]),
+        "total_calories": sum(part["calories"] for part in [meal.main, meal.side, meal.drink, meal.dessert])
+    }
+
